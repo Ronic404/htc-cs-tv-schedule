2dcfc42d5ba29b3157bd8198b3f674b9c26fa51d
ROnic404 <ronic@inbox.ru>
2020-06-12 14:27:48 +0400

    initial app

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'TV schedule',
+      filename: 'index.html',
+      template: './src/index.html',
+      favicon: './src/img/favicon.ico',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /(png|ico|jpe?g|gif)$/i,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
